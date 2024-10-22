69a43e0e8855309beeb20b7cb466dce417643376
vigneshs <vigneshs@melss.com>
2024-10-22 16:15:36 +0530

    Update Navbar.txs

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -16,11 +16,7 @@ export default function Navbar() {
 						gap={3}
 						display={{ base: "none", sm: "flex" }}
 					>
-						<img src='/react.png' alt='logo' width={50} height={50} />
-						<Text fontSize={"40"}>+</Text>
 						<img src='/go.png' alt='logo' width={40} height={40} />
-						<Text fontSize={"40"}>=</Text>
-						<img src='/explode.png' alt='logo' width={50} height={50} />
 					</Flex>
 
 					{/* RIGHT SIDE */}
